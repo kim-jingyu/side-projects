6fd7e69cc1eeba0d682581a753433f24b54d5a18
jingyu9804 <swc05161@gmail.com>
2023-06-22 23:47:22 +0900

    feat(springboot-developer): 블로그 글 추가 DTO 객체 생성

---
+++ src/main/java/me/jingyu/springbootdeveloper/dto/AddArticleRequest.java
@@ -0,0 +1,22 @@
+package me.jingyu.springbootdeveloper.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jingyu.springbootdeveloper.domain.Article;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+    private String title;
+    private String content;
+
+    // 블로그 글을 추가할 때, 저장할 엔티티로 변환하는 용도
+    public Article toEntity() {
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
