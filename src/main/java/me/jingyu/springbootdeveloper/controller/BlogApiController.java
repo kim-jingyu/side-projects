01c282dc3b9329197973aae623b6b3e1d89d98e3
jingyu9804 <swc05161@gmail.com>
2023-06-23 01:15:16 +0900

    feat(springboot-developer): 블로그 글 조회 api 생성

--- src/main/java/me/jingyu/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/jingyu/springbootdeveloper/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.jingyu.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.jingyu.springbootdeveloper.domain.Article;
 import me.jingyu.springbootdeveloper.dto.AddArticleRequest;
+import me.jingyu.springbootdeveloper.dto.ArticleResponse;
 import me.jingyu.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +24,17 @@ public class BlogApiController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    // 글 목록 조회
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(article -> new ArticleResponse(article))
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
