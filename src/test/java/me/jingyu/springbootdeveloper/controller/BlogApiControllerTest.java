64c9de2aed273ce390fe3b4f9d11266819d55d30
jingyu9804 <swc05161@gmail.com>
2023-06-23 02:10:44 +0900

    test(springboot-developer): 블로그 글 삭제 api 테스트

--- src/test/java/me/jingyu/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/jingyu/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -21,8 +21,8 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -48,6 +48,7 @@ class BlogApiControllerTest {
     @Test
     void addArticle() throws Exception {
         // given
+        // 블로그 글 추가에 필요한 요청 객체 생성
         final String url = "/api/articles";
         final String title = "title";
         final String content = "content";
@@ -56,11 +57,13 @@ class BlogApiControllerTest {
         String requestBody = objectMapper.writeValueAsString(userRequest);
 
         // when
+        // 블로그 글 추가 API에 요청을 보냄
         ResultActions result = mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(requestBody));
 
         // then
+        // 응답코드가 201 Created인지 확인
         result.andExpect(status().isCreated());
 
         List<Article> articles = blogRepository.findAll();
@@ -68,5 +71,85 @@ class BlogApiControllerTest {
         assertThat(articles.size()).isEqualTo(1);
         assertThat(articles.get(0).getTitle()).isEqualTo(title);
         assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+
+    // 블로그 글 목록 조회 api 테스트
+    @DisplayName("findAllArticles: 블로그 글 목록 조회 성공")
+    @Test
+    void findAllArticles() throws Exception {
+        // given
+        // 블로그 글 저장
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        // 목록 조회 API를 호출
+        ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        // 응답 코드가 0이고, 받환받은 값 중에 0번째 요소의 content와 title이 저장된 값과 같은지 확인
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+    }
+
+    // 블로그 글 하나 조회 api 테스트
+    @DisplayName("findArticle: 블로그 글 하나 조회 성공")
+    @Test
+    void findArticle() throws Exception {
+        // given
+        // 블로그 글 저장
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        // 저장한 블로그 글의 id값으로 API 호출
+        ResultActions result = mockMvc.perform(get(url, savedArticle.getId()));
+
+        // then
+        // 응답 코드가 200 OK이고, 반환받은 content와 title이 저장된 값과 같은지 확인
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.title").value(title));
+    }
+
+    @DisplayName("deleteArticle: 블로그 글 삭제 성공")
+    @Test
+    void deleteArticle() throws Exception {
+        // given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when
+        ResultActions result = mockMvc.perform(delete(url, savedArticle.getId()));
+
+        // then
+        result
+                .andExpect(status().isOk());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles).isEmpty();
     }
 }
