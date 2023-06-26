0a209ae6329bbea9c4114af31d9dea3217dabf74
JingyuKim <swc05161@gmail.com>
2023-06-27 02:32:37 +0900

    feat(photoalbum): 앨범 생성 기능 추가

--- src/main/java/com/photoalbum/controller/AlbumController.java
+++ src/main/java/com/photoalbum/controller/AlbumController.java
@@ -1,24 +1,32 @@
 package com.photoalbum.controller;
 
-import com.photoalbum.dto.AlbumResponseDto;
+import com.photoalbum.dto.AlbumDto;
 import com.photoalbum.service.AlbumService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/albums")
 @RequiredArgsConstructor
+@Slf4j
 public class AlbumController {
     private final AlbumService albumService;
 
     @GetMapping(value = "/{albumId}")
-    public ResponseEntity<AlbumResponseDto> getAlbum(@PathVariable Long albumId) {
-        AlbumResponseDto album = albumService.getAlbum(albumId);
+    public ResponseEntity<AlbumDto> getAlbum(@PathVariable Long albumId) {
+        AlbumDto album = albumService.getAlbum(albumId);
         return new ResponseEntity<>(album, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<AlbumDto> createAlbum(@RequestBody AlbumDto albumDto) throws IOException {
+        AlbumDto savedAlbumDto = albumService.createAlbum(albumDto);
+        return new ResponseEntity<>(savedAlbumDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/photoalbum/service/AlbumService.java
+++ src/main/java/com/photoalbum/service/AlbumService.java
@@ -1,7 +1,8 @@
 package com.photoalbum.service;
 
+import com.photoalbum.Constants;
 import com.photoalbum.domain.Album;
-import com.photoalbum.dto.AlbumResponseDto;
+import com.photoalbum.dto.AlbumDto;
 import com.photoalbum.mapper.AlbumMapper;
 import com.photoalbum.repository.AlbumRepository;
 import com.photoalbum.repository.PhotoRepository;
@@ -10,20 +11,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class AlbumService {
     private final AlbumRepository albumRepository;
     private final PhotoRepository photoRepository;
 
-    public AlbumResponseDto getAlbum(Long albumId) {
+    public AlbumDto getAlbum(Long albumId) {
         Optional<Album> album = albumRepository.findById(albumId);
 
         if (album.isPresent()) {
-            AlbumResponseDto dto = AlbumMapper.convertToDto(album.get());
+            AlbumDto dto = AlbumMapper.convertToDto(album.get());
             dto.setCount(photoRepository.countByAlbum_AlbumId(albumId));
             return dto;
         } else {
@@ -31,15 +35,34 @@ public class AlbumService {
         }
     }
 
-    public AlbumResponseDto getAlbumByName(String albumName) {
+    public AlbumDto getAlbumByName(String albumName) {
         Optional<Album> album = albumRepository.findByAlbumName(albumName);
 
         if (album.isPresent()) {
-            AlbumResponseDto dto = AlbumMapper.convertToDto(album.get());
+            AlbumDto dto = AlbumMapper.convertToDto(album.get());
             dto.setCount(photoRepository.countByAlbum_AlbumId(album.get().getAlbumId()));
             return dto;
         } else {
             throw new EntityNotFoundException(String.format("앨범명 %d으로 조회되는 값이 없습니다.", albumName));
         }
+    }
+
+    @Transactional
+    public AlbumDto createAlbum(AlbumDto albumDto) throws IOException {
+        Album album = AlbumMapper.convertToModel(albumDto);
+        albumRepository.save(album);
+        createAlbumDirectories(album);
+        return AlbumMapper.convertToDto(album);
+    }
+
+    /**
+     * 앨범 디렉토리 생성하기
+     * 파일과 경로 관련된 작업을 구현하기 위한 라이브러리 Paths, Files
+     * Paths.get 메서드 내 디렉토리 경로를 넣어줌으로써 Paths 객체를 만든다.
+     * Files.createDirectories 메서드 내 Paths 객체를 입력하면 폴더가 생성된다.
+     */
+    private void createAlbumDirectories(Album album) throws IOException {
+        Files.createDirectories(Paths.get(Constants.PATH_PREFIX + "/photos/original/" + album.getAlbumId()));
+        Files.createDirectories(Paths.get(Constants.PATH_PREFIX + "/photos/thumb/" + album.getAlbumId()));
     }
 }
