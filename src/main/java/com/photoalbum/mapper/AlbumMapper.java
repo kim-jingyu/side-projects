d90ed1f1db35af2a1a02dee59797d51abcd283a0
JingyuKim <swc05161@gmail.com>
2023-06-27 03:50:50 +0900

    feat(photoalbum): 앨범 리스트를 앨범 DTO 리스트로 converting 하는 메서드 추가

--- src/main/java/com/photoalbum/mapper/AlbumMapper.java
+++ src/main/java/com/photoalbum/mapper/AlbumMapper.java
@@ -3,6 +3,9 @@ package com.photoalbum.mapper;
 import com.photoalbum.domain.Album;
 import com.photoalbum.dto.AlbumDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class AlbumMapper {
     public static AlbumDto convertToDto(Album album) {
         AlbumDto dto = new AlbumDto();
@@ -18,5 +21,11 @@ public class AlbumMapper {
         album.setAlbumName(albumDto.getAlbumName());
         album.setCreatedAt(albumDto.getCreatedAt());
         return album;
+    }
+
+    public static List<AlbumDto> convertToDtoList(List<Album> albums) {
+        return albums.stream()
+                .map(AlbumMapper::convertToDto)
+                .toList();
     }
 }
