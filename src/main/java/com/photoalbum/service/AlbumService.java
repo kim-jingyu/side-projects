99be2bb0d5d4e7a21732caa1f5c7257f5743c2ae
JingyuKim <swc05161@gmail.com>
2023-06-26 20:59:41 +0900

    feat(photoalbum): 응답 DTO로 반환하게끔 수정

--- src/main/java/com/photoalbum/service/AlbumService.java
+++ src/main/java/com/photoalbum/service/AlbumService.java
@@ -1,24 +1,43 @@
 package com.photoalbum.service;
 
 import com.photoalbum.domain.Album;
+import com.photoalbum.dto.AlbumResponseDto;
+import com.photoalbum.mapper.AlbumMapper;
 import com.photoalbum.repository.AlbumRepository;
+import com.photoalbum.repository.PhotoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AlbumService {
     private final AlbumRepository albumRepository;
+    private final PhotoRepository photoRepository;
 
-    public Album getAlbum(Long albumId) {
-        return albumRepository.findById(albumId)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되지 않았습니다.", albumId)));
+    public AlbumResponseDto getAlbum(Long albumId) {
+        Optional<Album> album = albumRepository.findById(albumId);
+
+        if (album.isPresent()) {
+            AlbumResponseDto dto = AlbumMapper.convertToDto(album.get());
+            dto.setCount(photoRepository.countByAlbum_AlbumId(albumId));
+            return dto;
+        } else {
+            throw new EntityNotFoundException(String.format("앨범 아이디 %d로 조회되는 값이 없습니다.", albumId));
+        }
     }
 
-    public Album getAlbumByName(String albumName) {
-        return albumRepository.findByName(albumName)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("앨범명 %d로 조회되지 않았습니다.", albumName)));
+    public AlbumResponseDto getAlbumByName(String albumName) {
+        Optional<Album> album = albumRepository.findByName(albumName);
+
+        if (album.isPresent()) {
+            AlbumResponseDto dto = AlbumMapper.convertToDto(album.get());
+            dto.setCount(photoRepository.countByAlbum_AlbumId(album.get().getAlbumId()));
+            return dto;
+        } else {
+            throw new EntityNotFoundException(String.format("앨범명 %d으로 조회되는 값이 없습니다.", albumName));
+        }
     }
 }
