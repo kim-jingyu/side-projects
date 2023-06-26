87fc1fdaee8e1bf4984c6c5b7c5decfcc01d973e
JingyuKim <swc05161@gmail.com>
2023-06-26 21:00:30 +0900

    test(photoalbum): 앨범 - 사진 count 기능 잘 적용되었는지 테스트

--- src/test/java/com/photoalbum/service/AlbumServiceTest.java
+++ src/test/java/com/photoalbum/service/AlbumServiceTest.java
@@ -1,15 +1,16 @@
 package com.photoalbum.service;
 
 import com.photoalbum.domain.Album;
+import com.photoalbum.domain.Photo;
+import com.photoalbum.dto.AlbumResponseDto;
 import com.photoalbum.repository.AlbumRepository;
-import org.assertj.core.api.Assertions;
+import com.photoalbum.repository.PhotoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -18,6 +19,8 @@ class AlbumServiceTest {
     AlbumService albumService;
     @Autowired
     AlbumRepository albumRepository;
+    @Autowired
+    PhotoRepository photoRepository;
 
     @Test
     void getAlbum() {
@@ -26,7 +29,7 @@ class AlbumServiceTest {
 
         Album savedAlbum = albumRepository.save(album);
 
-        Album findAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
+        AlbumResponseDto findAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
         assertThat(findAlbum.getAlbumName()).isEqualTo("새로운 앨범");
     }
 
@@ -37,7 +40,22 @@ class AlbumServiceTest {
 
         Album savedAlbum = albumRepository.save(album);
 
-        Album findAlbum = albumService.getAlbumByName(savedAlbum.getAlbumName());
+        AlbumResponseDto findAlbum = albumService.getAlbumByName(savedAlbum.getAlbumName());
         assertThat(findAlbum.getAlbumName()).isEqualTo("새로운 앨범");
+    }
+
+    @Test
+    void testPhotoCount() {
+        Album album = new Album();
+        album.setAlbumName("새로운 앨범");
+        Album savedAlbum = albumRepository.save(album);
+
+        Photo photo = new Photo();
+        photo.setFileName("사진1");
+        photo.setAlbum(album);
+        photoRepository.save(photo);
+
+        AlbumResponseDto findAlbum = albumService.getAlbum(savedAlbum.getAlbumId());
+        assertThat(findAlbum.getCount()).isEqualTo(1);
     }
 }
