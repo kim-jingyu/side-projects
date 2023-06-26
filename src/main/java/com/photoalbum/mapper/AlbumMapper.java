921f23eb9ff358a19aeba73e6e3bf4133f3ddd5f
JingyuKim <swc05161@gmail.com>
2023-06-27 02:32:50 +0900

    feat(photoalbum): dto -> album converter 메서드 생성

--- src/main/java/com/photoalbum/mapper/AlbumMapper.java
+++ src/main/java/com/photoalbum/mapper/AlbumMapper.java
@@ -1,14 +1,22 @@
 package com.photoalbum.mapper;
 
 import com.photoalbum.domain.Album;
-import com.photoalbum.dto.AlbumResponseDto;
+import com.photoalbum.dto.AlbumDto;
 
 public class AlbumMapper {
-    public static AlbumResponseDto convertToDto(Album album) {
-        AlbumResponseDto dto = new AlbumResponseDto();
+    public static AlbumDto convertToDto(Album album) {
+        AlbumDto dto = new AlbumDto();
         dto.setAlbumId(album.getAlbumId());
         dto.setAlbumName(album.getAlbumName());
         dto.setCreatedAt(album.getCreatedAt());
         return dto;
+    }
+
+    public static Album convertToModel(AlbumDto albumDto) {
+        Album album = new Album();
+        album.setAlbumId(albumDto.getAlbumId());
+        album.setAlbumName(albumDto.getAlbumName());
+        album.setCreatedAt(albumDto.getCreatedAt());
+        return album;
     }
 }
