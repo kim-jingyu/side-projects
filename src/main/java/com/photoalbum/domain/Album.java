371175009a08dad7b19aaec7b2b86288b573710f
JingyuKim <swc05161@gmail.com>
2023-06-26 20:31:20 +0900

    feat(photoalbum): 생성자 접근 범위 변경

--- src/main/java/com/photoalbum/domain/Album.java
+++ src/main/java/com/photoalbum/domain/Album.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter @Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Entity
 @Table(name = "album", schema = "photo_album", uniqueConstraints = {@UniqueConstraint(columnNames = "album_id")})
 public class Album {
