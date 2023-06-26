d927397a522701ac111a21b49b8d949aa8b79828
JingyuKim <swc05161@gmail.com>
2023-06-26 20:30:24 +0900

    feat(photoalbum): repository 생성

---
+++ src/main/java/com/photoalbum/repository/AlbumRepository.java
@@ -0,0 +1,10 @@
+package com.photoalbum.repository;
+
+import com.photoalbum.domain.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+    Optional<Album> findByName(String albumName);
+}
