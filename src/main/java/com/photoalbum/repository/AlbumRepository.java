f55429d8ffeaa9e29b87e44ab0c4b85716712a02
JingyuKim <swc05161@gmail.com>
2023-06-27 03:53:26 +0900

    feat(photoalbum): 앨범 이름을 포함하면서 앨범명으로 오름차순 정렬, 생성 날짜의 최신순으로 정렬하는 앨범 리스트를 꺼내오는 메서드 추가

--- src/main/java/com/photoalbum/repository/AlbumRepository.java
+++ src/main/java/com/photoalbum/repository/AlbumRepository.java
@@ -3,8 +3,13 @@ package com.photoalbum.repository;
 import com.photoalbum.domain.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     Optional<Album> findByAlbumName(String albumName);
+
+    List<Album> findByAlbumNameContainingOrderByAlbumNameAsc(String keyword);
+
+    List<Album> findByAlbumNameContainingOrderByCreatedAtDesc(String keyword);
 }
