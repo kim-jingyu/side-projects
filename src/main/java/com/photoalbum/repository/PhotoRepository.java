12100b01f0bdde3f4e0ac904c3a173061897a87a
JingyuKim <swc05161@gmail.com>
2023-06-27 16:33:42 +0900

    feat(photoalbum): 앨범 아이디별로 최신 4장의 이미지를 가져오는 메서드 생성

--- src/main/java/com/photoalbum/repository/PhotoRepository.java
+++ src/main/java/com/photoalbum/repository/PhotoRepository.java
@@ -3,6 +3,11 @@ package com.photoalbum.repository;
 import com.photoalbum.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PhotoRepository extends JpaRepository<Photo,Long> {
     int countByAlbum_AlbumId(Long albumId);
+
+    // 앨범 아이디별로 최신 4장의 이미지를 가져오는 메서드
+    List<Photo> findTop4ByAlbum_AlbumIdOrderByUploadedAtDesc(Long albumId);
 }
