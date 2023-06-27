cdf36ae1a29c657a485b0d9cc0e18b9310252fbc
JingyuKim <swc05161@gmail.com>
2023-06-27 22:23:22 +0900

    feat(photoalbum): 파일 업로드 경로 및 썸네일 사이즈 상수 선언

--- src/main/java/com/photoalbum/Constants.java
+++ src/main/java/com/photoalbum/Constants.java
@@ -2,4 +2,7 @@ package com.photoalbum;
 
 public class Constants {
     public static final String PATH_PREFIX = "/Users/jingyu/side-projects/photoalbum";
+    public static final int THUMB_SIZE = 300;
+    public static final String ORIGINAL_PATH = PATH_PREFIX + "/photos/original";
+    public static final String THUMB_PATH = PATH_PREFIX + "/photos/thumb";
 }
