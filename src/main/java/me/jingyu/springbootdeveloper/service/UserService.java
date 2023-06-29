cf15144ebd26038bc65a6718c76092657768ea4b
JingyuKim <swc05161@gmail.com>
2023-06-29 20:41:38 +0900

    feat(springboot-developer): 트랜잭션 설정 및 RefreshToken 구현에서 필요한 메서드 구현

--- src/main/java/me/jingyu/springbootdeveloper/service/UserService.java
+++ src/main/java/me/jingyu/springbootdeveloper/service/UserService.java
@@ -6,17 +6,26 @@ import me.jingyu.springbootdeveloper.dto.AddUserRequest;
 import me.jingyu.springbootdeveloper.repository.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional
     public Long save(AddUserRequest dto) {
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 .password(bCryptPasswordEncoder.encode(dto.getPassword()))
                 .build()).getId();
+    }
+
+    // RefreshToken 구현에서 필요
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저 아이디로 검색되는 유저가 없습니다."));
     }
 }
