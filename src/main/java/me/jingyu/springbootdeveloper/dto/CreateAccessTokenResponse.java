3da5bef93eb9f6895842487ca2273233eaf39817
JingyuKim <swc05161@gmail.com>
2023-06-29 21:24:08 +0900

    fix(springboot-developer): 토큰 생성 응답 DTO 수정

--- src/main/java/me/jingyu/springbootdeveloper/dto/CreateAccessTokenResponse.java
+++ src/main/java/me/jingyu/springbootdeveloper/dto/CreateAccessTokenResponse.java
@@ -1,10 +1,11 @@
 package me.jingyu.springbootdeveloper.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 // 토큰 생성 응답 DTO
-@Getter @Setter
+@AllArgsConstructor
+@Getter
 public class CreateAccessTokenResponse {
     private String accessToken;
 }
