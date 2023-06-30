40522bb9c4e17a7ca4070dfae7c2a35bddffc48a
JingyuKim <swc05161@gmail.com>
2023-06-30 22:18:57 +0900

    feat: 글 수정,삭제 기능에 글쓴이 확인 기능 추가

--- src/main/java/me/jingyu/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/jingyu/springbootdeveloper/service/BlogService.java
@@ -5,6 +5,7 @@ import me.jingyu.springbootdeveloper.domain.Article;
 import me.jingyu.springbootdeveloper.dto.AddArticleRequest;
 import me.jingyu.springbootdeveloper.dto.UpdateArticleRequest;
 import me.jingyu.springbootdeveloper.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import java.util.List;
 public class BlogService {
     private final BlogRepository blogRepository;
 
+    @Transactional
     public Article save(AddArticleRequest request, String username) {
         return blogRepository.save(request.toEntity(username));
     }
@@ -29,8 +31,13 @@ public class BlogService {
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 
+    @Transactional
     public void delete(Long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found" + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
@@ -38,8 +45,17 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 요청 헤더에 전달된 토큰을 이용하여 게시글을 작성한 유저인지 검증
+    private void authorizeArticleAuthor(Article article) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(username)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
