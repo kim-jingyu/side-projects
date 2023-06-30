c0057584854877e189170fa6282afde11d178be2
JingyuKim <swc05161@gmail.com>
2023-06-30 21:35:38 +0900

    feat: 글에 글쓴이 추가하기

--- src/main/java/me/jingyu/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/jingyu/springbootdeveloper/dto/AddArticleRequest.java
@@ -13,10 +13,11 @@ public class AddArticleRequest {
     private String content;
 
     // 블로그 글을 추가할 때, 저장할 엔티티로 변환하는 용도
-    public Article toEntity() {
+    public Article toEntity(String author) {
         return Article.builder()
                 .title(title)
                 .content(content)
+                .author(author)
                 .build();
     }
 }
