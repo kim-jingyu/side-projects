eed01b5d04af20f80403f789bb1144d6812bcea7
JingyuKim <swc05161@gmail.com>
2023-06-30 18:18:22 +0900

    feat: 인증에 성공할시에 실행할 핸들러

--- src/main/java/me/jingyu/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/me/jingyu/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
@@ -42,12 +42,24 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         // 1. RefreshToken 생성 -> 저장 -> 쿠키에 저장
         String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
-        saveRefreshToken(user.getId(), refreshToken);
-        addRefreshTokenToCookie(request, response, refreshToken);
+        saveRefreshToken(user.getId(), refreshToken);   // RefreshToken을 데이터베이스에 유저 아이디와 함께 저장
+        addRefreshTokenToCookie(request, response, refreshToken);   // 클라이언트에서 액세스 토큰이 만료되면 재발급 요청하도록 쿠키에 RefreshToken 저장
 
         // 2. 액세스 토큰 생성 -> path에 액세스 토큰 추가
         String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
-        String targetUrl = getTargetUrl(accessToken);
+        String targetUrl = getTargetUrl(accessToken);   // 리다이렉트 경로의 쿼리 파라미터에 AccessToken을 추가
+
+        // 3. 인증관련 설정값과 쿠키를 제거
+        clearAuthenticationAttributes(request, response);   // 인증 프로세스를 진행하면서 세션과 쿠키에 임시로 저장해둔 인증 관련 데이터를 제거
+
+        // 4. 리다이렉트
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    // 인증 관련 설정값과 쿠키 제거
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        authorizationRequestRepository.removeAuthorizationRequest(request, response);   // OAuth 인증을 위해 저장된 정보도 삭제
     }
 
     // 생성된 리프레시 토큰을 전달받아 데이터베이스에 저장한다.
@@ -66,6 +78,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
     }
 
+    // 액세스 토큰을 리다이렉트 path에 추가한다.
     private String getTargetUrl(String accessToken) {
         return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
                 .queryParam("token", accessToken)
