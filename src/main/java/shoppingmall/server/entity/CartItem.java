0d70b6df269faa93a593c4ec7caf52493555ed80
JingyuKim <swc05161@gmail.com>
2023-07-06 14:26:40 +0900

    feat: 상품-장바구니 엔티티 생성

---
+++ src/main/java/shoppingmall/server/entity/CartItem.java
@@ -0,0 +1,22 @@
+package shoppingmall.server.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+}
