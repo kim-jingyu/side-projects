639441b587b76d0694e34edefc82f7d0f6b28c6c
JingyuKim <swc05161@gmail.com>
2023-07-06 13:26:32 +0900

    feat: 장바구니 엔티티 생성

---
+++ src/main/java/shoppingmall/server/entity/Cart.java
@@ -0,0 +1,16 @@
+package shoppingmall.server.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
