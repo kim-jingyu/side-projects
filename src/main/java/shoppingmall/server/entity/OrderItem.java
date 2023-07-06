e13f8509697435e764b963938529f7c7fef6c8f2
JingyuKim <swc05161@gmail.com>
2023-07-06 15:02:05 +0900

    feat: 상품 주문 엔티티 생성

---
+++ src/main/java/shoppingmall/server/entity/OrderItem.java
@@ -0,0 +1,23 @@
+package shoppingmall.server.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Orders order;
+
+    private int count;
+    private int orderPrice;
+}
