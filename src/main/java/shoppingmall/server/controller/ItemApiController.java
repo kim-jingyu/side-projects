4baecaaa0f7e6af7745f3a343dcb1f32005ee49f
JingyuKim <swc05161@gmail.com>
2023-07-06 19:10:27 +0900

    feat: 상품 저장 컨트롤러 생성

---
+++ src/main/java/shoppingmall/server/controller/ItemApiController.java
@@ -0,0 +1,46 @@
+package shoppingmall.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import shoppingmall.server.dto.ItemRequestDto;
+import shoppingmall.server.dto.ItemResponseDto;
+import shoppingmall.server.service.ItemService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ItemApiController {
+    private final ItemService itemService;
+
+    // 상품 등록
+    @PostMapping(value = "/admin/item")
+    public ResponseEntity<List<ItemResponseDto>> addItem(@RequestBody @Validated ItemRequestDto itemRequestDto, BindingResult bindingResult, @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .build();
+        }
+
+        List<ItemResponseDto> itemResponseDtoList = new ArrayList<>();
+
+        try {
+            itemResponseDtoList.add(itemService.saveItem(itemRequestDto, itemImgFileList));
+        } catch (IOException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+
+        return ResponseEntity
+                .ok()
+                .body(itemResponseDtoList);
+    }
+}
