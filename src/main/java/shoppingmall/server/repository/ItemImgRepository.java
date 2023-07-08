1e09f2e4100ea8f65f3b0de7c7cebc8660406123
JingyuKim <swc05161@gmail.com>
2023-07-08 20:24:43 +0900

    feat: 상품 상세페이지 조회 기능 추가

--- src/main/java/shoppingmall/server/repository/ItemImgRepository.java
+++ src/main/java/shoppingmall/server/repository/ItemImgRepository.java
@@ -3,5 +3,8 @@ package shoppingmall.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shoppingmall.server.entity.ItemImg;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+    List<ItemImg> findByItem_ItemIdOrderByItemImgIdAsc(Long itemId);
 }
--- src/main/java/shoppingmall/server/service/ItemService.java
+++ src/main/java/shoppingmall/server/service/ItemService.java
@@ -1,41 +1,67 @@
 package shoppingmall.server.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import shoppingmall.server.dto.ItemImgDto;
 import shoppingmall.server.dto.ItemRequestDto;
 import shoppingmall.server.dto.ItemResponseDto;
 import shoppingmall.server.entity.Item;
 import shoppingmall.server.entity.ItemImg;
+import shoppingmall.server.repository.ItemImgRepository;
 import shoppingmall.server.repository.ItemRepository;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Slf4j
 public class ItemService {
     private final ItemRepository itemRepository;
     private final ItemImgService itemImgService;
+    private final ItemImgRepository itemImgRepository;
 
+    @Transactional
     public ItemResponseDto saveItem(ItemRequestDto itemRequestDto, List<MultipartFile> itemImgFileList) throws IOException {
         Item savedItem = itemRepository.save(Item.builder().requestDto(itemRequestDto).build());
 
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
         for (int i = 0; i < itemImgFileList.size(); i++) {
             if (i == 0) {
-                itemImgService.saveItemImg(ItemImg.createItemImg(savedItem, "Y"), itemImgFileList.get(i));
+                itemImgDtoList.add(itemImgService.saveItemImg(ItemImg.createItemImg(savedItem, null, null, null, "Y"), itemImgFileList.get(i)));
             } else {
-                itemImgService.saveItemImg(ItemImg.createItemImg(savedItem, "N"), itemImgFileList.get(i));
+                itemImgDtoList.add(itemImgService.saveItemImg(ItemImg.createItemImg(savedItem, null, null, null,"N"), itemImgFileList.get(i)));
             }
         }
 
         return ItemResponseDto
                 .builder()
                 .item(savedItem)
+                .itemImgDtoList(itemImgDtoList)
+                .build();
+    }
+
+    public ItemResponseDto getItemDetail(Long itemId) {
+        List<ItemImg> itemImgList = itemImgRepository.findByItem_ItemIdOrderByItemImgIdAsc(itemId);
+
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+        for (ItemImg itemImg : itemImgList) {
+            itemImgDtoList.add(new ItemImgDto(itemImg));
+        }
+
+        Item findItem = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return ItemResponseDto
+                .builder()
+                .item(findItem)
+                .itemImgDtoList(itemImgDtoList)
                 .build();
     }
 }
