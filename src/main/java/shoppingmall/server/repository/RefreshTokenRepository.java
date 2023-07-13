b9e366b206ace84e653f27a941f8975ed9c3b2a9
JingyuKim <swc05161@gmail.com>
2023-07-14 02:12:27 +0900

    feat: 리프레시 토큰 도메인 생성

---
+++ src/main/java/shoppingmall/server/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package shoppingmall.server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shoppingmall.server.entity.RefreshToken;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
