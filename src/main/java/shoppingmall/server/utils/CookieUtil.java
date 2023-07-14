23dd019220b5620f7fdc673c4c4ac20d3d88985f
JingyuKim <swc05161@gmail.com>
2023-07-14 18:14:16 +0900

    feat: 쿠키 유틸 생성

---
+++ src/main/java/shoppingmall/server/utils/CookieUtil.java
@@ -0,0 +1,48 @@
+package shoppingmall.server.utils;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
+
+@Slf4j
+public class CookieUtil {
+    // 쿠키 추가
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+    // 쿠키 삭제
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return;
+        }
+
+        for (Cookie cookie : cookies) {
+            if (name.equals(cookie.getName())) {
+                cookie.setValue(" ");
+                cookie.setPath("/");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+    }
+
+    // 객체를 직렬화해 쿠키의 값으로 변환
+    public static String serialize(Object object) {
+        byte[] serialized = SerializationUtils.serialize(object);   // 객체 -> byte
+        return Base64.getUrlEncoder().encodeToString(serialized);   // byte -> json
+    }
+
+    // 쿠키를 역직렬화해 쿠키의 값으로 변환
+    public static <T> T deserialize(Cookie cookie, Class<T> cls) {
+        return cls.cast(SerializationUtils.deserialize(Base64.getUrlDecoder().decode(cookie.getValue())));
+    }
+}
