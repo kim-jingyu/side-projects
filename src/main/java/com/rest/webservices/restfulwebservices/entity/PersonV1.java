53dd3b7c47931181a99620a1133d2967312e9baf
JingyuKim <swc05161@gmail.com>
2023-07-16 02:01:40 +0900

    feat: URI Versioning V1

---
+++ src/main/java/com/rest/webservices/restfulwebservices/entity/PersonV1.java
@@ -0,0 +1,12 @@
+package com.rest.webservices.restfulwebservices.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class PersonV1 {
+    private String name;
+}
