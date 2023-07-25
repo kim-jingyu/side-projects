638e8ac22957d91ac8e927b6f36d613c5d90c9bc
JingyuKim <swc05161@gmail.com>
2023-07-25 18:06:57 +0900

    feat: itemImgResponseList 필드 추가

--- src/main/java/com/myproject/todayhouse/item/dto/response/ItemResponse.java
+++ src/main/java/com/myproject/todayhouse/item/dto/response/ItemResponse.java
@@ -5,18 +5,22 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter @Setter
 public class ItemResponse {
     private String brandName;
     private String itemName;
     private int price;
     private int stockQuantity;
+    private List<ItemImgResponse> itemImgResponseList;
 
     @Builder
-    public ItemResponse(Item item) {
+    public ItemResponse(Item item, List<ItemImgResponse> itemImgResponseList) {
         this.brandName = item.getBrandName();
         this.itemName = item.getItemName();
         this.price = item.getPrice();
         this.stockQuantity = item.getStockQuantity();
+        this.itemImgResponseList = itemImgResponseList;
     }
 }
