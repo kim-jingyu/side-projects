69f279f7892500183b96bb74116ec26ee7c4bddb
JingyuKim <swc05161@gmail.com>
2023-07-25 17:38:50 +0900

    feat: 파일 저장 기능 구현

--- src/main/java/com/myproject/todayhouse/item/util/FileService.java
+++ src/main/java/com/myproject/todayhouse/item/util/FileService.java
@@ -1,4 +1,50 @@
 package com.myproject.todayhouse.item.util;
 
+import com.myproject.todayhouse.item.dto.request.ItemImgRequest;
+import com.myproject.todayhouse.item.dto.response.ItemImgResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 public class FileService {
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    public ItemImgRequest storeFile(MultipartFile multipartFile, String representYn) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return null;
+        }
+
+        String uploadFileName = multipartFile.getOriginalFilename();
+        String ext = getExt(uploadFileName);
+
+        String storedFileName = getStoredFileName(ext);
+        String storedFileUrl = getStoredFileUrl(storedFileName);
+
+        multipartFile.transferTo(new File(storedFileUrl));
+
+        return ItemImgRequest.builder()
+                .uploadFileName(uploadFileName)
+                .storedFileName(storedFileName)
+                .storedFileUrl(storedFileUrl)
+                .representYn(representYn)
+                .build();
+    }
+
+    private String getExt(String uploadFileName) {
+        int pos = uploadFileName.lastIndexOf(".");
+        return uploadFileName.substring(pos + 1);
+    }
+
+    private String getStoredFileName(String ext) {
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String getStoredFileUrl(String storedFileName) {
+        return itemImgLocation + storedFileName;
+    }
 }
