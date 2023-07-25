46e506fe573adea001753d6d2940d092da32e07b
JingyuKim <swc05161@gmail.com>
2023-07-25 17:40:00 +0900

    feat: 상품 이미지 저장 기능 구현

--- src/main/java/com/myproject/todayhouse/item/service/ItemImgService.java
+++ src/main/java/com/myproject/todayhouse/item/service/ItemImgService.java
@@ -1,17 +1,33 @@
 package com.myproject.todayhouse.item.service;
 
+import com.myproject.todayhouse.item.domain.ItemImg;
+import com.myproject.todayhouse.item.dto.request.ItemImgRequest;
+import com.myproject.todayhouse.item.dto.response.ItemImgResponse;
 import com.myproject.todayhouse.item.repository.ItemImgRepository;
+import com.myproject.todayhouse.item.util.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemImgService {
     private ItemImgRepository itemImgRepository;
+    private FileService fileService;
 
     @Transactional
-    public void saveItemImg() {
+    public ItemImgResponse saveItemImg(MultipartFile multipartFile, String representYn) throws IOException {
+        ItemImgRequest itemImgRequest = fileService.storeFile(multipartFile, representYn);
+
+        ItemImg itemImg = ItemImg.createItemImg(itemImgRequest);
+        itemImgRepository.save(itemImg);
+
+        return ItemImgResponse.builder()
+                .itemImg(itemImg)
+                .build();
     }
 }
