0f725195a2514688c5e826941c9b05039ba7991a
JingyuKim <swc05161@gmail.com>
2023-07-25 18:51:28 +0900

    feat: 상품 저장 기능 구현

--- src/main/java/com/myproject/todayhouse/item/service/ItemService.java
+++ src/main/java/com/myproject/todayhouse/item/service/ItemService.java
@@ -3,12 +3,18 @@ package com.myproject.todayhouse.item.service;
 import com.myproject.todayhouse.item.domain.Item;
 import com.myproject.todayhouse.item.domain.ItemSellStatus;
 import com.myproject.todayhouse.item.dto.request.ItemRequest;
+import com.myproject.todayhouse.item.dto.response.ItemImgResponse;
 import com.myproject.todayhouse.item.dto.response.ItemResponse;
+import com.myproject.todayhouse.item.exception.ItemNotFoundException;
 import com.myproject.todayhouse.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,25 +22,24 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+    private final ItemImgService itemImgService;
 
     @Transactional
-    public ItemResponse saveItem(ItemRequest itemRequest) {
-        Optional<Item> findItem = itemRepository.findByItemNameAndBrandName(itemRequest.getItemName(), itemRequest.getBrandName());
-        if (findItem.isPresent()) {
-            Item item = findItem.get();
-            item.updateItem(itemRequest);
-            return ItemResponse.builder()
-                    .item(item)
-                    .build();
+    public ItemResponse saveItem(ItemRequest itemRequest, List<MultipartFile> itemImgFileList) throws IOException {
+        Item item = Item.createItem(itemRequest);
+
+        List<ItemImgResponse> itemImgResponseList = new ArrayList<>();
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            if (i == 0) {
+                itemImgResponseList.add(itemImgService.saveItemImg(itemImgFileList.get(i), "Y"));
+            } else {
+                itemImgResponseList.add(itemImgService.saveItemImg(itemImgFileList.get(i), "N"));
+            }
         }
 
-        Item savedItem = itemRepository.save(Item.builder()
-                .itemRequest(itemRequest)
-                .itemSellStatus(ItemSellStatus.SELL)
-                .build());
-
         return ItemResponse.builder()
-                .item(savedItem)
+                .item(item)
+                .itemImgResponseList(itemImgResponseList)
                 .build();
     }
 }
