cf32de044379ef9edabfc38052e3281cb61aea18
kim-jingyu <swc05161@gmail.com>
2023-07-29 00:55:01 +0900

    feat: 썸네일 필드 추가

--- src/main/java/com/myproject/todayhouse/item/dto/request/ItemImgRequest.java
+++ src/main/java/com/myproject/todayhouse/item/dto/request/ItemImgRequest.java
@@ -7,13 +7,19 @@ import lombok.Setter;
 @Getter @Setter
 public class ItemImgRequest {
     private String uploadFileName;
+    private String thumbFileName;
+    private String thumbFileUrl;
     private String storedFileName;
     private String storedFileUrl;
+    private Boolean representYn;
 
     @Builder
-    public ItemImgRequest(String uploadFileName, String storedFileName, String storedFileUrl) {
+    public ItemImgRequest(String uploadFileName, String thumbFileName, String thumbFileUrl, String storedFileName, String storedFileUrl, Boolean representYn) {
         this.uploadFileName = uploadFileName;
+        this.thumbFileName = thumbFileName;
+        this.thumbFileUrl = thumbFileUrl;
         this.storedFileName = storedFileName;
         this.storedFileUrl = storedFileUrl;
+        this.representYn = representYn;
     }
 }
