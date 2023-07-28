14b7ca87dea5b9e03aa3024ed46688563d093255
kim-jingyu <swc05161@gmail.com>
2023-07-29 00:54:32 +0900

    fix: 필드 수정 및 예외 메시지 추가

--- src/main/java/com/myproject/todayhouse/item/dto/request/ItemCreateRequest.java
+++ src/main/java/com/myproject/todayhouse/item/dto/request/ItemCreateRequest.java
@@ -2,21 +2,22 @@ package com.myproject.todayhouse.item.dto.request;
 
 import com.myproject.todayhouse.item.domain.ItemSellStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class ItemCreateRequest {
-    @NotBlank
+    @NotBlank(message = "브랜드명을 입력해주세요!")
     private String brandName;
-    @NotBlank
+    @NotBlank(message = "상품명을 입력해주세요!")
     private String itemName;
-    @NotBlank
+
+    @NotNull(message = "상품 가격을 입력해주세요!")
     private int price;
-    @NotBlank
+    @NotNull(message = "상품 재고를 입력해주세요!")
     private int stockQuantity;
-    @NotBlank
+
     private String itemDetail;
-    @NotBlank
     private ItemSellStatus itemSellStatus;
 }
