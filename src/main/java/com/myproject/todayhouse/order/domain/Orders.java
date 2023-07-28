ed3c5a525a8245d19020a50dcde79f78921b9e9d
kim-jingyu <swc05161@gmail.com>
2023-07-28 18:22:08 +0900

    feat: 주문 도메인 생성

---
+++ src/main/java/com/myproject/todayhouse/order/domain/Orders.java
@@ -0,0 +1,28 @@
+package com.myproject.todayhouse.order.domain;
+
+import com.myproject.todayhouse.common.BaseTimeEntity;
+import com.myproject.todayhouse.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Orders extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
