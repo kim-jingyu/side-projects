85fac973e9cfa4ddb25f628edb3b28a249007ed8
kim-jingyu <swc05161@gmail.com>
2023-07-29 00:53:43 +0900

    feat: BaseTimeEntity 상속

--- src/main/java/com/myproject/todayhouse/order/domain/Delivery.java
+++ src/main/java/com/myproject/todayhouse/order/domain/Delivery.java
@@ -1,5 +1,6 @@
 package com.myproject.todayhouse.order.domain;
 
+import com.myproject.todayhouse.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Delivery {
+public class Delivery extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long deliveryId;
