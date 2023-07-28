c2fd8ce1a3c0d72e70d72bd180efae1429b7eb4a
kim-jingyu <swc05161@gmail.com>
2023-07-29 00:55:06 +0900

    feat: 썸네일 필드 추가

--- src/main/java/com/myproject/todayhouse/item/dto/response/ItemImgResponse.java
+++ src/main/java/com/myproject/todayhouse/item/dto/response/ItemImgResponse.java
@@ -9,16 +9,20 @@ import lombok.Setter;
 public class ItemImgResponse {
     private Long itemImgId;
     private String uploadFileName;
+    private String thumbFileName;
+    private String thumbFileUrl;
     private String storedFileName;
     private String storedFileUrl;
-    private String representYn;
+    private boolean representYn;
 
     @Builder
     public ItemImgResponse(ItemImg itemImg) {
         this.itemImgId = itemImg.getItemImgId();
         this.uploadFileName = itemImg.getUploadFileName();
+        this.thumbFileName = itemImg.getThumbFileName();
+        this.thumbFileUrl = itemImg.getThumbFileUrl();
         this.storedFileName = itemImg.getStoredFileName();
         this.storedFileUrl = itemImg.getStoredFileUrl();
-        this.representYn = itemImg.getRepresentYn();
+        this.representYn = itemImg.isRepresentYn();
     }
 }
