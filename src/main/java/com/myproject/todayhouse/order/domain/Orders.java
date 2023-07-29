d94d967ab1669319823da0833bcb6cd2583e2d97
JingyuKim <swc05161@gmail.com>
2023-07-29 16:17:34 +0900

    feat: 생성 메서드 및 연관관계 메서드 추가

--- src/main/java/com/myproject/todayhouse/order/domain/Orders.java
+++ src/main/java/com/myproject/todayhouse/order/domain/Orders.java
@@ -7,7 +7,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
+@Table(name = "order")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Orders extends BaseTimeEntity {
@@ -25,4 +29,41 @@ public class Orders extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    // 연관관계 메서드
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    public void setOrderItem(OrderItem orderItem) {
+        orderItemList.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    private Orders(OrderStatus orderStatus, Member member) {
+        this.orderStatus = orderStatus;
+        this.member = member;
+    }
+
+    public static Orders createOrder(Member member, Delivery delivery, OrderItem orderItem) {
+        Orders order = new Orders(OrderStatus.ORDER, member);
+        order.setDelivery(delivery);
+        order.setOrderItem(orderItem);
+        return order;
+    }
+
+    // 비즈니스 메서드
+
+    public int totalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItemList) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
