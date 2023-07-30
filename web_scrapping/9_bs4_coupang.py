c294dcce2d987c855a33d841fd29019a871a8e4d
JingyuKim <swc05161@gmail.com>
2023-07-30 21:02:42 +0900

    feat: beautiful soup - coupang

---
+++ web_scrapping/9_bs4_coupang.py
@@ -0,0 +1,12 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=3&rocketAll=false&searchIndexingToken=1=9&backgroundColor="
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+
+items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
+print(items)
