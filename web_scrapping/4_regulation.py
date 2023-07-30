041eb1648a25fd22d9a505084781e0c769764ddb
JingyuKim <swc05161@gmail.com>
2023-07-30 18:28:01 +0900

    feat: 정규식

--- web_scrapping/4_regulation.py
+++ web_scrapping/4_regulation.py
@@ -8,7 +8,11 @@ p = re.compile("ca.e")
 
 def print_match(param):
     if param:
-        print(param.group())
+        print("param.group() :", param.group())  # group -> 일치하는 문자열 반환
+        print("param.string :", param.string)  # string -> 입력받은 문자열
+        print("param.start() :", param.start())  # 일치하는 문자열의 시작 index
+        print("param.end() :", param.end())  # 일치하는 문자열의 끝 index
+        print("param.span() :", param.span())  # 일치하는 문자열의 시작, 끝 index
     else:
         print("매칭되지 않았습니다.")
 
@@ -17,4 +21,10 @@ def print_match(param):
 m = p.match("caffe")
 print_match(m)
 
-# search : 주어진 문자열의 처음부터 일치하는지 확인
+# search : 주어진 문자열 중에 일치하는게 있는지 확인
+s = p.search("good care")
+print_match(s)
+
+# findall : 일치하는 모든 것을 리스트 형태로 반환
+lst = p.findall("good care cafe")
+print(lst)
