6c61800ae877f362df07c7e4c6ef18658b5c1b05
kim-jingyu <swc05161@gmail.com>
2023-08-26 20:51:36 +0900

    feat: 영화 목록 받아오기 API 생성

--- src/main/java/com/movie/controller/MovieController.java
+++ src/main/java/com/movie/controller/MovieController.java
@@ -1,11 +1,14 @@
 package com.movie.controller;
 
 import com.movie.crawler.Crawler;
+import com.movie.crawler.MovieService;
 import com.movie.domain.Movie;
+import com.movie.dto.MovieResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -15,11 +18,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieController {
     private final Crawler crawler;
+    private final MovieService movieService;
 
     @GetMapping("/movie/crawl")
     public ResponseEntity initDb() throws IOException {
         List<Movie> movies = crawler.getMovies();
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(movies);
+    }
+
+    @GetMapping("/movie/list")
+    public ResponseEntity getMovieList(@RequestParam(defaultValue = "likes") String sortMode, @RequestParam(defaultValue = "false") boolean trashMode) {
+        List<Movie> movies = movieService.getMovieList(trashMode, sortMode);
+
+        MovieResponse movieResponse = new MovieResponse("success", "영화 리스트 GET 요청 완료!", movies);
+
+        return ResponseEntity.ok()
+                .body(movieResponse);
     }
 }
