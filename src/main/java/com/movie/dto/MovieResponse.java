52cb4a6296ce755fb7b08f005fb46f17ee26a5ab
kim-jingyu <swc05161@gmail.com>
2023-08-27 01:50:52 +0900

    feat: 영화 응답 DTO로 변경

--- src/main/java/com/movie/dto/MovieResponse.java
+++ src/main/java/com/movie/dto/MovieResponse.java
@@ -1,16 +1,12 @@
 package com.movie.dto;
 
-import com.movie.domain.Movie;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Getter @Setter
 public class MovieResponse {
     private String result;
     private String msg;
-    private List<Movie> movieList;
 }
