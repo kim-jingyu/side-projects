e35856b44f045e566a5973b12309cc5d84e83378
JingyuKim <swc05161@gmail.com>
2023-08-27 21:10:18 +0900

    feat: UserController 생성

---
+++ src/main/kotlin/com/library/controller/user/UserController.kt
@@ -0,0 +1,36 @@
+package com.library.controller.user
+
+import com.library.dto.user.UserCreateRequest
+import com.library.dto.user.UserResponse
+import com.library.dto.user.UserUpdateRequest
+import com.library.service.user.UserService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(
+    private val userService: UserService
+) {
+    @PostMapping("/user")
+    fun saveUser(@RequestBody request: UserCreateRequest) {
+        userService.saveUser(request)
+    }
+
+    @GetMapping("/user")
+    fun getUsers(): List<UserResponse> = userService.getUsers()
+
+    @PutMapping("/user")
+    fun updateUserName(@RequestBody request: UserUpdateRequest) {
+        userService.updateUserName(request)
+    }
+
+    @DeleteMapping("/user")
+    fun deleteUser(@RequestParam name: String) {
+        userService.deleteUser(name)
+    }
+}
