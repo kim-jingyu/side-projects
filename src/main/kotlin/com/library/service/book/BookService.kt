8aaa387008b5993908b6080f540aaa362918fa6a
JingyuKim <swc05161@gmail.com>
2023-08-27 20:01:21 +0900

    feat: BookService 생성

---
+++ src/main/kotlin/com/library/service/book/BookService.kt
@@ -0,0 +1,42 @@
+package com.library.service.book
+
+import com.library.domain.book.Book
+import com.library.domain.book.BookRepository
+import com.library.domain.user.UserRepository
+import com.library.domain.user.loanhistory.UserLoanHistoryRepository
+import com.library.dto.book.BookLoanRequest
+import com.library.dto.book.BookRequest
+import com.library.dto.book.BookReturnRequest
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class BookService(
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
+) {
+    @Transactional
+    fun saveBook(request: BookRequest) {
+        val newBook = Book(request.name)
+        bookRepository.save(newBook)
+    }
+
+    @Transactional
+    fun loanBook(request: BookLoanRequest) {
+        val book = bookRepository.findByName(request.bookName).orElseThrow(::IllegalArgumentException)
+        if (userLoanHistoryRepository.findByBookNameAndIsReturn(request.bookName, false) != null) {
+            throw IllegalArgumentException("진작 대출되어 있는 책입니다.")
+        }
+
+        val user = userRepository.findByName(request.userName).orElseThrow(::IllegalArgumentException)
+        user.loanBook(book)
+    }
+
+    @Transactional
+    fun returnBook(request: BookReturnRequest) {
+        val user = userRepository.findByName(request.userName).orElseThrow(::IllegalArgumentException)
+        user.returnBook(request.bookName)
+    }
+}
