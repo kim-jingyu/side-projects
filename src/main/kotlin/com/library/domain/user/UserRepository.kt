57fd2c19578065047c9c48be184051e8780725c2
JingyuKim <swc05161@gmail.com>
2023-08-27 19:16:34 +0900

    feat: UserRepository 생성

---
+++ src/main/kotlin/com/library/domain/user/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.library.domain.user
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByName(name: String) : Optional<User>
+}
