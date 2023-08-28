13ece54e859067a726fd608bd804b0070e168bd5
kim-jingyu <swc05161@gmail.com>
2023-08-28 13:36:44 +0900

    test: UserServiceTest 생성

---
+++ src/test/kotlin/com/library/service/user/UserServiceTest.kt
@@ -0,0 +1,12 @@
+package com.library.service.user
+
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserServiceTest @Autowired constructor(
+
+) {
+
+}
