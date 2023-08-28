e5ebb4993ce03ccaee45592ac62919ba05dd7c4c
JingyuKim <swc05161@gmail.com>
2023-08-28 15:57:45 +0900

    fix: user -> users 테이블명 변경 (예약어 관련)

--- src/main/kotlin/com/library/domain/user/User.kt
+++ src/main/kotlin/com/library/domain/user/User.kt
@@ -8,8 +8,10 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "users")
 class User(
     var name: String,
     val age: Int?,
