4a918deece7d7985402e7f49744113a2aab34af5
kim-jingyu <swc05161@gmail.com>
2023-08-30 01:22:13 +0900

    feat: 책 분야 추가하기

--- src/main/kotlin/com/library/domain/book/Book.kt
+++ src/main/kotlin/com/library/domain/book/Book.kt
@@ -9,6 +9,8 @@ import jakarta.persistence.Id
 class Book(
     val name: String,
 
+    val type: String,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
@@ -17,6 +19,22 @@ class Book(
     init {
         if (name.isBlank()) {
             throw IllegalArgumentException("이름은 비어있을 수 없습니다.")
+        }
+    }
+
+    // 테스트에 이용할 객체를 만드는 함수 - Object Mother
+    // 이렇게 생겨난 테스트용 객체를 Test Fixture 라고 부른다.
+    companion object {
+        fun fixture (
+            name: String = "책 이름",
+            type: String = "COMPUTER",
+            id: Long? = null,
+        ): Book {
+            return Book(
+                name = name,
+                type = type,
+                id = id,
+            )
         }
     }
 }
--- src/main/kotlin/com/library/dto/book/BookRequest.kt
+++ src/main/kotlin/com/library/dto/book/BookRequest.kt
@@ -2,4 +2,5 @@ package com.library.dto.book
 
 data class BookRequest(
     var name: String,
+    val type: String,
 )
