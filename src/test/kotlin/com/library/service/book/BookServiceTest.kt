78fcc38c8b1e39ce0c71accd54dd91aa182b91a0
kim-jingyu <swc05161@gmail.com>
2023-08-31 02:20:31 +0900

    test: 책 통계 보여주기 - 프로덕션 코드

--- src/test/kotlin/com/library/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/library/service/book/BookServiceTest.kt
@@ -5,11 +5,13 @@ import com.library.domain.book.BookRepository
 import com.library.domain.book.BookType
 import com.library.domain.user.User
 import com.library.domain.user.UserRepository
+import com.library.domain.user.loanhistory.UserLoanHistory
 import com.library.domain.user.loanhistory.UserLoanHistoryRepository
 import com.library.domain.user.loanhistory.UserLoanStatus
 import com.library.dto.book.BookLoanRequest
 import com.library.dto.book.BookRequest
 import com.library.dto.book.BookReturnRequest
+import com.library.dto.book.BookStatResponse
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
@@ -106,5 +108,51 @@ class BookServiceTest @Autowired constructor(
         val loanHistories = loanHistoryRepository.findAll()
         assertThat(loanHistories).hasSize(1)
         assertThat(loanHistories[0].status).isEqualTo(UserLoanStatus.RETURNED)
+    }
+
+    @Test
+    @DisplayName("책 대여 권수를 정상 확인")
+    fun countLoanedBookTest() {
+        // given
+        val savedUser = userRepository.save(User.fixture("A"))
+        loanHistoryRepository.saveAll(
+            listOf(
+                UserLoanHistory.fixture(savedUser, "책1"),
+                UserLoanHistory.fixture(savedUser, "책2", UserLoanStatus.RETURNED),
+                UserLoanHistory.fixture(savedUser, "책3", UserLoanStatus.RETURNED),
+            )
+        )
+
+        // when
+        val result = bookService.countLoanedBook()
+
+        // then
+        assertThat(result).isEqualTo(1)
+    }
+
+    @Test
+    @DisplayName("")
+    fun getBookStaticsTest() {
+        // given
+        bookRepository.saveAll(
+            listOf(
+                Book.fixture("클린 코드", BookType.COMPUTER),
+                Book.fixture("토비의 스프링", BookType.COMPUTER),
+                Book.fixture("양자역학", BookType.SCIENCE),
+            )
+        )
+
+        // when
+        val results = bookService.getBookStatistics()
+        println("results = $results")
+
+        // then
+        assertThat(results).hasSize(2)
+        assertCount(results, BookType.COMPUTER, 2L)
+        assertCount(results, BookType.SCIENCE, 1L)
+    }
+
+    private fun assertCount(results: List<BookStatResponse>, type: BookType, count: Long) {
+        assertThat(results.first { result -> result.type == type }.count).isEqualTo(count)
     }
 }
